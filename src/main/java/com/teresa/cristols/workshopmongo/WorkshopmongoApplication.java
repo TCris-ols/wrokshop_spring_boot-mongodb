259fdf0b8aa07d88a3e58622d113b6ec50fbd21b
Teresa Cristina O Souza <teresa.cristols@gmail.com>
2021-01-07 14:34:23 -0300

    Entity User e REST funcionando

--- src/main/java/com/teresa/cristols/workshopmongo/WorkshopmongoApplication.java
+++ src/main/java/com/teresa/cristols/workshopmongo/WorkshopmongoApplication.java
@@ -1,4 +1,4 @@
-package com.teresa.cristols;
+package com.teresa.cristols.workshopmongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;

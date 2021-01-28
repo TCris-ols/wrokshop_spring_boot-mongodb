a40a2d9f197efb6160dc9490e82f01eb263a2801
Teresa Cristina O Souza <teresa.cristols@gmail.com>
2021-01-28 14:05:38 -0300

    Consulta simples com query methods

--- src/main/java/com/teresa/cristols/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/teresa/cristols/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.teresa.cristols.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,6 @@ import com.teresa.cristols.workshopmongo.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
-
+	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }

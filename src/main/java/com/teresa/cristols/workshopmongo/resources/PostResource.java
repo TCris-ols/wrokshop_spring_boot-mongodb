aeebaf1d4b33d2413a7eb400a927180131d581c5
Teresa Cristina O Souza <teresa.cristols@gmail.com>
2021-01-30 19:22:27 -0300

    Consulta com varios criterios

--- src/main/java/com/teresa/cristols/workshopmongo/resources/PostResource.java
+++ src/main/java/com/teresa/cristols/workshopmongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.teresa.cristols.workshopmongo.resources;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -35,4 +36,20 @@ public class PostResource {
 		
 		return  ResponseEntity.ok().body(list);
 	}
+	
+	@RequestMapping(value="/searchfull",method=RequestMethod.GET)
+	public ResponseEntity<List<Post>> searchFull(
+			@RequestParam(value="text", defaultValue="") String text,
+			@RequestParam(value="minDate", defaultValue="") String minDate,
+			@RequestParam(value="maxDate", defaultValue="") String maxDate)
+	{
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		
+		List<Post> list = service.searchFull(text, min, max);
+		
+		return  ResponseEntity.ok().body(list);
+	}
+
 }
